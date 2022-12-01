59b1c98e0fb427a04bf8d1b0f4b9f8d80bc798b6
napatel641999 <napatel641999@outlook.com>
2022-12-01 12:35:54 -0500

    Practical 28

--- Practical 28/EmployeeManagementCommonHelper/ViewModel/UserLogin.cs
+++ Practical 28/EmployeeManagementCommonHelper/ViewModel/UserLogin.cs
@@ -7,7 +7,10 @@ namespace EmployeeManagementCommonHelper.ViewModel
 {
     public class UserLogin
     {
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "The email address is not valid")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
